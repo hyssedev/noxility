d18f873176dd2d3521b3d36550c4096c8834b95f
SauciucIonut <ionut.sauciuc2003@gmail.com>
2020-12-06 15:15:02 +0200

    first commit
    base bot with eval and cog functions

---
+++ bot.py
@@ -0,0 +1,53 @@
+from discord.ext import commands
+import discord, os, asyncio, logging
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('nox'), intents=intents)
+
+bot.remove_command("help")
+
+for cog in os.listdir("cogs"):
+    if cog.endswith(".py"):
+        if cog != "__init__.py":
+            try:
+                cog = f"cogs.{cog.replace('.py', '')}"
+                bot.load_extension(cog)
+                print(f"{cog} loaded.")
+            except Exception as e:
+                print(f"{cog} couldn't be loaded.")
+                raise e        
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, cog=None):
+    """
+    Usage: <prefix> reload <cog>
+    It reloads a cog. You can use this if a cog is not working correctly.
+    """
+    cogs = []
+    for cogg in os.listdir("cogs"):
+        if cogg.endswith(".py"):
+            if cogg != "__init__.py":
+                cogs.append(cogg)
+    if cog is None: await ctx.send(f"Please specify which cog to reload. Available cogs: {cogs}.")
+    else:
+        try:
+            bot.unload_extension(f"cogs.{cog}")
+            bot.load_extension(f"cogs.{cog}")
+            await ctx.send(f"Am reincarcat {cog}.")
+            print(f"Am reincarcat {cog}.")
+        except Exception as e:
+            print(f"{cog} nu poate fi reincarcat.")
+            raise e
+
+"""
+# logging system
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+"""
+
+bot.run("")
---
+++ cogs/eval.py
@@ -0,0 +1,67 @@
+from discord.ext import commands
+import asyncio, traceback, discord, inspect, textwrap, importlib, io, os, re, sys, copy, time, subprocess
+from contextlib import redirect_stdout
+
+class Eval (commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_result = None
+    
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    @commands.command(name='eval')
+    @commands.is_owner()
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
+def setup(bot):
+    bot.add_cog(Eval(bot))
