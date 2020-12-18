9858915cf62890938e7a1120260f891f5ecf845d
SauciucIonut <ionut.sauciuc2003@gmail.com>
2020-12-18 14:17:02 +0200

    quick fix

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord, os, asyncio, logging
 import cogs._utils
 
-bot = commands.Bot(command_prefix=['nox ', 'Nox ', 'NOX ', 'noxility', 'Noxility'], owner_id=199375184057073664, intents=discord.Intents.all(), help_command=cogs._utils.EmbedHelpCommand(), description=f"Noxility is a powerful bot with all kinds of commands, ready to make your server more fun & enjoyable. It currently has {len([i for i in bot.walk_commands()])} commands.")
+bot = commands.Bot(command_prefix=['nox ', 'Nox ', 'NOX ', 'noxility', 'Noxility'], owner_id=199375184057073664, intents=discord.Intents.all(), help_command=cogs._utils.EmbedHelpCommand(), description="Noxility is a powerful bot with all kinds of commands, ready to make your server more fun & enjoyable.")
 logging.basicConfig(level=logging.INFO) 
 
 for cog in os.listdir("cogs"):
