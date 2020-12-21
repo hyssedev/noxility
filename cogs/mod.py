7dce58b481750da81afaef61005e2a6dcc2d3b37
SauciucIonut <ionut.sauciuc2003@gmail.com>
2020-12-21 19:39:39 +0200

    enabled kick and ban, completely reworked ban, it now works with members who are not in the current guild

--- cogs/mod.py
+++ cogs/mod.py
@@ -12,16 +12,16 @@ class Mod (commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(enabled=False)
+    @commands.command()
     @commands.guild_only()
     @commands.bot_has_permissions(kick_members=True)
     @commands.has_guild_permissions(kick_members=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         """Kicks specified member."""
-        if await utils.utils.role_hierarchy(ctx, member): return
-        if ctx.guild.me.top_role <= member.top_role:
-            return await ctx.send(f"Error, you can't do this because my role is lower than **{member.name}**.")
+        if ctx.guild.me.top_role <= member.top_role: return await ctx.send(f"Error, you can't do this because my role is lower than **{member.name}**.")
+        if ctx.author.id != ctx.guild.owner.id:
+            if ctx.author.top_role <= member.top_role: return await ctx.send(f"Error, you can't do this because **{member.name}** has a higher role than you do.")
         await ctx.guild.kick(discord.Object(id=member.id), reason=reason)
         await ctx.send(f"✅ Successfully **{str(ctx.command.name)}ed** {member.name}.")
 
@@ -38,20 +38,40 @@ class Mod (commands.Cog):
         await ctx.message.delete()
         await ctx.message.channel.purge(limit=int(amount))
         await ctx.send(f"✅ Sucesfully deleted **{int(amount)}** message{utils.utils.plural_check(int(amount))}!", delete_after=5)
-
-    @commands.command(enabled=False, hidden=True)
+    
+    """
+    # disabled, this command does not support banning people that are not in the current guild
+    @commands.command()
     @commands.guild_only()
     @commands.bot_has_permissions(ban_members=True)
     @commands.has_permissions(ban_members=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def ban(self, ctx, user: discord.User):
-        if await utils.utils.role_hierarchy(ctx, user): return
+    async def ban(self, ctx, member: discord.Member):
+        Bans specified user, currently only works on members that are on the current server.
+        if ctx.guild.me.top_role <= member.top_role: return await ctx.send(f"Error, you can't do this because my role is lower than **{member.name}**.")
+        if ctx.author.id != ctx.guild.owner.id:
+            if ctx.author.top_role <= member.top_role: return await ctx.send(f"Error, you can't do this because **{member.name}** has a higher role than you do.")
         try:
-            await ctx.guild.ban(discord.Object(id=user.id))
+            await ctx.guild.ban(discord.Object(id=member.id))
+            await ctx.send(f"✅ Sucesfully banned **{member.name}#{member.discriminator}**!")
         except:
-            return await ctx.send(f"Error, can't ban member {user.name}.")
+            return await ctx.send(f"Error, can't ban member {member.name}.")
+    """
 
-
+    @commands.command()
+    @commands.guild_only()
+    @commands.bot_has_permissions(ban_members=True)
+    @commands.has_permissions(ban_members=True)
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def ban(self, ctx, member: utils.utils.MemberID):
+        """Bans specified user. Works even if the member is not in the current guild."""
+        try:
+            if ctx.author.id != ctx.guild.owner.id:
+                if ctx.author.top_role <= member.top_role: return await ctx.send(f"Error, you can't do this because **{member.name}** has a higher role than you do.")
+        except:
+            pass
+        await ctx.guild.ban(member)
+        await ctx.send(f"✅ Sucesfully banned **{member}**!")
 
 def setup(bot):
     bot.add_cog(Mod(bot))
--- utils/utils.py
+++ utils/utils.py
@@ -270,3 +270,39 @@ def degToCompass(num):
     val=int((num/22.5)+0.5)
     arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
     return arr[(val % 16)]
+
+async def get_or_fetch_member(self, guild, member_id):
+    member = guild.get_member(member_id)
+    if member is not None:
+        return member
+
+    try:
+        member = await guild.fetch_member(member_id)
+    except discord.HTTPException:
+        return None
+
+    members = await guild.query_members(limit=1, user_ids=[member_id], cache=True)
+    if not members:
+        return None
+    return members[0]
+
+def can_execute_action(ctx, user, target):
+    return user.id == ctx.bot.owner_id or \
+        user == ctx.guild.owner or \
+        user.top_role > target.top_role
+
+class MemberID(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            m = await commands.MemberConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                member_id = int(argument, base=10)
+            except ValueError:
+                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+            else:
+                m = await get_or_fetch_member(self, ctx.guild, member_id)
+                if m is None:
+                    # hackban case
+                    return type('_Hackban', (), {'id': member_id, '__str__': lambda s: f'Member ID {s.id}'})()
+        return m
