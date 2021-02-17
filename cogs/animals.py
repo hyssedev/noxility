30e414f34cdfd1675fea1613635b60fbea02ccd7
SauciucIonut <ionut.sauciuc2003@gmail.com>
2021-02-17 13:37:11 +0200

    new `koalafact` command

--- cogs/animals.py
+++ cogs/animals.py
@@ -180,5 +180,19 @@ class Animals (commands.Cog):
         except:
             raise discord.errors.Forbidden
 
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def koalafact(self, ctx):
+        """Tells you a random koala fact."""
+        try:
+            async with aiohttp.ClientSession(headers={}) as cs:
+                async with cs.get('https://some-random-api.ml/facts/koala') as r:
+                    res = await r.json()
+                    embed = discord.Embed(colour=0xf2c203)
+                    embed.add_field(name="**Here is your random koala fact. 🐨**", value=res['fact'])
+                    await ctx.send(embed=embed)
+        except:
+            raise discord.errors.Forbidden
+
 def setup(bot):
     bot.add_cog(Animals(bot))
