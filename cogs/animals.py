1494c7e37f3719cc19ec5b983c00c472217bfed1
SauciucIonut <ionut.sauciuc2003@gmail.com>
2021-02-17 13:10:00 +0200

    new `panda` command

--- cogs/animals.py
+++ cogs/animals.py
@@ -54,5 +54,19 @@ class Animals (commands.Cog):
         except:
             raise discord.errors.Forbidden
 
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def panda(self, ctx):
+        """Shows you a panda picture."""
+        try:
+            async with aiohttp.ClientSession(headers={}) as cs:
+                async with cs.get('https://some-random-api.ml/img/panda') as r:
+                    res = await r.json()
+                    embed = discord.Embed(description="**Here is your panda picture. 🐼**", colour=0xf2c203)
+                    embed.set_image(url=res['link'])
+                    await ctx.send(embed=embed)
+        except:
+            raise discord.errors.Forbidden
+
 def setup(bot):
     bot.add_cog(Animals(bot))
